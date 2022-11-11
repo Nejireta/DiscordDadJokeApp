6323df99924a4e8dda451e663a1fd71303a5e1b8
Nejireta <68238073+Nejireta@users.noreply.github.com>
2022-11-11 15:56:51 +0100

    Added config in appsettings

---
+++ DiscordDadJokeApp/AppSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace DiscordDadJokeApp {
+    internal sealed class AppSettings {
+        public sealed class Settings {
+            public string ChannelId { get; set; }
+            public string Webhook { get; set; }
+        }
+
+        private const string ConfigFileName = "appsettings.json";
+        private const string ConfigSection = "Discord";
+
+        internal static Settings Get() {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(ConfigFileName, optional: false);
+
+            IConfiguration configuration = configurationBuilder.Build();
+            return configuration.GetSection(ConfigSection).Get<Settings>();
+        }
+    }
+}
