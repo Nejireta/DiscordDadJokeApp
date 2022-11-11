f107cf454cc89bad4de22dcff8b99f937ed813f2
Nejireta <68238073+Nejireta@users.noreply.github.com>
2022-11-12 00:24:18 +0100

    Update Format.cs

--- DiscordDadJokeApp/Format.cs
+++ DiscordDadJokeApp/Format.cs
@@ -4,25 +4,24 @@
             //[DisplayName("Success")]
             public bool success { get; set; } //success
             public DadJokeBodyFormat body { get; set; } //body
-            public sealed class DadJokeBodyFormat {
-                public string _id { get; set; } //_id
-                public string setup { get; set; } //setup
-                public string punchline { get; set; } //punchline
-                public string type { get; set; } //type
-                public string[] likes { get; set; } //likes
-                public AuthorSubclass author { get; set; } //author
-                public bool approved { get; set; } //approved
-                public int date { get; set; } //date
-                public bool NSFW { get; set; } //NSFW
-                public string shareableLink { get; set; } //shareableLink
-
-                public sealed class AuthorSubclass {
-                    public string Nanameme { get; set; } //name
-                    public int id { get; set; } //id
-                }
-            }
+        }
+        public sealed class DadJokeBodyFormat {
+            public string _id { get; set; } //_id
+            public string setup { get; set; } //setup
+            public string punchline { get; set; } //punchline
+            public string type { get; set; } //type
+            public string[] likes { get; set; } //likes
+            public AuthorSubclass author { get; set; } //author
+            public bool approved { get; set; } //approved
+            public int date { get; set; } //date
+            public bool NSFW { get; set; } //NSFW
+            public string shareableLink { get; set; } //shareableLink
         }
 
+        public sealed class AuthorSubclass {
+            public string name { get; set; } //name
+            public int id { get; set; } //id
+        }
         public sealed class DiscordChatFormat {
             public Embeds[] embeds { get; set; }
         }
